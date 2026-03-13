02e9fdbacd5ed4f0e3d592bce71cc841f45b814f
Cocktail Design <dimikravets@yandex.ru>
2026-03-13 20:38:21 +0700

    Обновить sync route: moysklad-bundle-item

--- src/api/moysklad-bundle-item/routes/sync.ts
+++ src/api/moysklad-bundle-item/routes/sync.ts
@@ -1,13 +1,11 @@
 // backend/src/api/moysklad-bundle-item/routes/sync.ts
 
-import controller from "../controllers/sync";
-
 export default {
   routes: [
     {
       method: "POST",
       path: "/moysklad/sync/bundle-items",
-      handler: controller.syncOne,
+      handler: "moysklad-bundle-item.syncOne",
       config: {
         auth: false,
       },
